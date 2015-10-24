26058a2b3440f62eec5acdf088eaccbaf30ea603
ha1fha1f <half.ldk.hiz@gmail.com>
2015-10-24 18:52:39 +0900

    Initial Commit

---
+++ paintPallet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  paintPallet
+//
+//  Created by 山口智生 on 2015/10/24.
+//  Copyright © 2015年 Tomoki Yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
